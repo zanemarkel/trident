8f5e3eeecbeacfb67339c8445da2efcf38004d67
m154500 <m154500@usna.edu>
2014-02-20 09:37:35 -0500

    Started trial script

---
+++ learn/trial.py
@@ -0,0 +1,38 @@
+""" Run a single machine learning trial. """
+###############################################################
+# Author:       Zane Markel
+# Created:      20 FEB 2013
+#
+# Name:         trial
+# Description:  Controls a single machine learning trial.
+#               Basically, this program puts together all the 
+#               other programs needed to run a trial.
+#
+#               
+#               
+#               
+#               
+#               
+###############################################################
+
+def main():
+    # parse cmd line args
+    # TODO: make option for loading intermediate data to skip steps that have
+    # been done in previous trials
+
+    # Select data to read
+
+    # Preprocess data
+
+    # Load appropriate learning algorithm
+
+    # Train
+
+    # Test
+
+    # Analyze -- FScore, acc, learning curves
+    
+    print 'hello world'
+
+if __name__ == '__main__':
+    main()
