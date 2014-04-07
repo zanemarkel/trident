77d297f1c2e500bcb0cba0a24313441cfbc115ec
m154500 <m154500@usna.edu>
2014-04-06 21:21:40 -0400

    All files ready for this spring 2014 experiments

---
+++ exp/spring14runs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# spring14runs.sh
+# Zane Markel
+# 6 APR 2014
+# This file will run all the trials necessary for the spring 2014 paper
+
+# usage: ./spring14runs.sh DB TRIAL.PY RESULTS_DIR SUMMARY_FILE GRAPHS_DIR
+
+# -h option
+if [ $1 = "-h" ]
+then
+    echo "usage: ./spring14runs.sh DB TRIAL.PY RESULTS_DIR SUMMARY_FILE GRAPHS_DIR"
+    exit
+fi
+
+db=$1
+trial=$2
+resdir=$3
+sumfi=$4
+gradir=$5
+
+# just a test
+#echo $db' '$resdir' '$sumfi
+
+samples=50000 # Keep the number of samples constant
+algs=('nb' 'dt' 'dte')
+malfracs=('0.5' '0.2' '0.1' '0.01' '0.001')
+seeds=('3574682466873' '1946873456497' '32546815137583')
+
+echo "accuracy,precision,recall,f1,algorithm,malfrac,seed" > $sumfi
+for i in ${algs[@]}
+do
+    for j in ${malfracs[@]}
+    do
+        for k in ${seeds[@]}
+        do
+            name="res-"$i"-"$j"-"$k
+            echo $name 
+            
+            # Var to print a graph for decision trees
+            g=""
+            if [[ $i = "dt" || $i = "dte" ]]
+            then
+                g="-g "$gradir$name"-graph.pdf"
+            fi
+
+            echo "python $trial -d $db -s $k -n $samples -m $j -a $i $g > $resdir$name"
+            #python summarize.py -c "$k" $resdir$name >> $sumfi
+        done
+    done
+done
--- exp/summarize.py
+++ exp/summarize.py
@@ -7,6 +7,9 @@ trial. """
 # Name:         trial
 # Description:  Parses the output of trial.py and prints 
 #               a line of csv
+#
+#               NOTE: the first line is a dict of the options, consider using
+#               import ast \n ast.literal_eval(line) in order to get it
 #               
 ###############################################################
 
@@ -19,18 +22,36 @@ def main():
     # Command line option Parser
     clparser = argparse.ArgumentParser()
     clparser.add_argument("results", type=file, \
-        help='file containing a trial\'s results')
+            help='file containing a trial\'s results')
     clparser.add_argument("-c", "--comments", type=str, \
-        help='comments to add to line')
-    clparser.parse_args()
+            help='comments to add to line')
+    args = clparser.parse_args()
 
-    # Parse the file
+    summarize(args)
 
-# Make a line
+def summarize(args):
+    ''' The function that actually does all the labor. '''
 
-# Append the comments
+    # Parse the file for averages
+    summary = ''
+    with args.results as res:
+        
+        # The first line is a dict string
+        _ = res.readline()
+        # The next line in the header information
+        _ = res.readline()
 
-# Output the csv line
+        # Make a line
+        for line in res.readlines():
+            linemod = line.strip().split()
+            summary += '{}'.format(linemod[1]) + ','
+
+
+        # Append the comments
+        summary += ' {}'.format(args.comments)
+
+        # Output the csv line
+        print(summary)
 
 if __name__ == '__main__':
     main()
