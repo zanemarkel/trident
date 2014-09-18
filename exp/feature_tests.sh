b539ff2e8fc4fec3ae6e11aba786bf6f0e4db7a8
m154500 <m154500@usna.edu>
2014-09-18 15:15:01 -0400

    Script for testing isolated features: feature_tests.sh

---
+++ exp/feature_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# feature_tests.sh
+# Zane Markel
+# 18 SEP 2014
+# This file runs an entropy-based decision tree trial on all databases in a directory
+# The intention is to get decision tree graphs of isolated features in order to
+# determine the information gain of that feature.
+
+# usage: ./feature_tests.sh DIRECTORY TRIAL.PY RESULTS_DIR SUMMARY_FILE GRAPHS_DIR
+
+# -h option
+if [ $1 = "-h" ]
+then
+    echo "usage: ./feature_tests.sh DIRECTORY TRIAL.PY RESULTS_DIR SUMMARY_FILE GRAPHS_DIR"
+    exit
+fi
+
+dir=$1
+trial=$2
+resdir=$3
+sumfi=$4
+gradir=$5
+
+samples=10000
+alg='dte'
+malfracs='0.5 0.5'
+seed='1029384756'
+
+for file in $dir/*
+do
+    bname=$(basename $file)
+    name=$bname"-test"
+    graphstr="-g "$gradir$name"-graph.pdf"
+    echo $name
+    python $trial -s $seed -n $samples -m $malfracs $g $file $alg > $resdir$name
+    python summarize.py -c "$alg,$malfracs,$seed,$bname" $resdir$name >> $sumfi
+done
