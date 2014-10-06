58d8e401b79508838e28a60342d3b63c6e9a44c0
m154500 <m154500@usna.edu>
2014-10-06 15:23:35 -0400

    Fixed feature_tests.sh to print graphs

--- exp/feature_tests.sh
+++ exp/feature_tests.sh
@@ -28,12 +28,12 @@ malfracs='0.5 0.5'
 seed='1029384756'
 
 echo "accuracy,precision,recall,f1,algorithm,malfrac_tr,malfrac_te,seed,feature" > $sumfi
-for file in $dir/*
+for file in $dir*
 do
     bname=$(basename $file)
     name=$bname"-test"
     graphstr="-g "$gradir$name"-graph.pdf"
     echo $name
-    python $trial -s $seed -n $samples -m $malfracs $g $file $alg > $resdir$name
+    python $trial -s $seed -n $samples -m $malfracs $graphstr $file $alg > $resdir$name
     python summarize.py -c "$alg,$malfracs,$seed,$bname" $resdir$name >> $sumfi
 done
